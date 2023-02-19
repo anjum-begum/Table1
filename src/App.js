4aa75e794a5888616786d8080ae1705245e64a60
Anjum <anjum.appo78@gmail.com>
2023-02-19 17:13:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const URL = "https://jsonplaceholder.typicode.com/posts";
+  const [data , getData] = useState([]);
 
+  useEffect(() => {
+    tableData();
+  }, [])
+
+  const tableData = () => {
+    axios.get(URL).then((response) => {
+        console.log(response);
+      })
+  }
+  return (
+    <div>
+      <h1>How Display API data in Table in React JS</h1
+      <tbody>
+        <tr>
+          <th>User ID</th>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Descripation</th>
+        </tr>
+        {data.length>0 && data.map((item, i) => (
+          <tr key={i}>
+            <td>{item.userId}</td>
+            <td>{item.id}</td>
+            <td>{item.title}</td>
+            <td>{item.body}</td>
+          </tr>
+        ))
+}
+</tbody>
+</div>
 export default App;
